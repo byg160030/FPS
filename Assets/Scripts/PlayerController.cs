7c18bcbb4082a3e83034f3e625918b39f34d3c91
Brice Yuuki Gibson <byg160030@utdallas.edu>
2020-12-15 20:06:01 -0600

    Camera Movement and Character Movement
    Basic camera movement character movement is completed, gravity is not yet added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,10 @@ public class PlayerController : MonoBehaviour
 
     public Transform camTrans;
 
+    public float mouseSensetivity;
+    public bool invertX;
+    public bool invertY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,16 +24,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        charCon.Move(moveInput);
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");
+
+        moveInput = horiMove + vertMove;
+        moveInput.Normalize();
+        moveInput = moveInput * moveSpeed;
+
+        charCon.Move(moveInput * Time.deltaTime);
 
         //control camera rotation
-        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensetivity;
+
+        if(invertX)
+        {
+            mouseInput.x = -mouseInput.x;
+        }
+
+        if (invertY)
+        {
+            mouseInput.y = -mouseInput.y;
+        }
 
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
 
-        camTrans.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(mouseInput.y, 0f, 0f));
+        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
     }
 }
