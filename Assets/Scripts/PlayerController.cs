472770531fe4c0edd7e2f468f3792da1840692cd
Brice Yuuki Gibson <byg160030@utdallas.edu>
2020-12-23 00:20:45 -0600

    Gravity and Jumping added
    Gravity is added and the player can now jump.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed, gravityModifier, jumpPower;
     public CharacterController charCon;
 
     private Vector3 moveInput;
@@ -14,6 +14,10 @@ public class PlayerController : MonoBehaviour
     public float mouseSensetivity;
     public bool invertX;
     public bool invertY;
+
+    private bool canJump;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +31,32 @@ public class PlayerController : MonoBehaviour
         //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
+        //store y velocity
+        float yStore = moveInput.y;
+
         Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
         Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");
 
         moveInput = horiMove + vertMove;
         moveInput.Normalize();
         moveInput = moveInput * moveSpeed;
+
+        moveInput.y = yStore;
+
+        moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
+
+        if(charCon.isGrounded)
+        {
+            moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
+
+        canJump = Physics.OverlapSphere(groundCheckPoint.position, .25f, whatIsGround).Length > 0;
+
+        //Handle Jumping
+        if(Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            moveInput.y = jumpPower;
+        }
 
         charCon.Move(moveInput * Time.deltaTime);
 
